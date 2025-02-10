6dbfb95b99d645b0961f457dcf5cf681e4eff5d9
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-02-11 00:01:40 +0100

    Add babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "presets": [
+    "babel-preset-expo"
+  ],
+  "plugins": [
+    [
+      "module-resolver",
+      {
+        "root": [
+          "./src"
+        ],
+        "extensions": [
+          ".ios.js",
+          ".android.js",
+          ".js",
+          ".ts",
+          ".tsx",
+          ".json"
+        ],
+        "alias": {
+          "@": "./src"
+        }
+      }
+    ]
+  ]
+}
